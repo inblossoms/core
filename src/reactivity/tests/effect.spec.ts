f31330758fc827ac40774aed3731ecf82ebcae0f
MEIYA-yl <2648297163@qq.com>
2022-04-07 14:15:32 +0800

    实现 effect 返回 runner

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -17,4 +17,21 @@ describe("effect", () => {
 		user.age++;
 		expect(nextAge).toBe(22)
 	})
+
+	it("should return runner when call effect", () => {
+		/*  实现：完善effect的部分功能
+					当调用 effect(fn) 功能时,会返回一个函数（这里称为runner），
+					当调用runner的时候会执行fn,runner可以拿到fn函数内部的返回值
+		*/
+		let foo = 10;
+		const runner = effect(() => {
+			foo++
+			return "foo"
+		})
+
+		expect(foo).toBe(11)
+		const r = runner();
+		expect(foo).toBe(12)
+		expect(r).toBe("foo")
+	})
 })
