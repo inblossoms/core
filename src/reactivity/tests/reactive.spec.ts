75664d07be7515c897e5f3ac4377abd39643bac4
MEIYA-yl <2648297163@qq.com>
2022-04-07 22:56:17 +0800

    优化 stop 功能

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -2,6 +2,29 @@ import { reactive, readonly, isReactive, isReadonly } from "../reactive";
 
 describe("readonly reactive", () => {
 
+	it("realize reactive and readonly Nested object conversion", () => {
+		const original = { foo: 1 }
+		const observed = reactive(original)
+
+		expect(observed).not.toBe(original)
+		expect(observed.foo).toBe(original)
+		expect(isReactive(observed)).toBe(true)
+		expect(isReactive(original)).toBe(false)
+	})
+
+	test("nested reactive", () => {
+		const original = {
+			nested: {
+				foo: 1
+			},
+			array: [{ bar: 2 }]
+		}
+		const observed = reactive(original)
+	})
+
+
+
+
 	it("test isReactive", () => {
 		const original = { foo: 1 }
 		const observed = reactive(original)
