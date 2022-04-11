aded9c369002764aa5c7224cfc453c5b11711877
MEIYA-yl <2648297163@qq.com>
2022-04-11 20:17:07 +0800

    -实现 shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index";
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -9,10 +10,14 @@ export function render(vnode, container) {
 
 
 function patch(vnode, container) {
+	// ShapeFlags 描述节点类型，标识当前的vnode有哪几种flag
 	// console.log(vnode.type);
-	if (typeof vnode.type === "string") {
+	const { shapeFlag } = vnode
+	if (shapeFlag & ShapeFlags.ELEMENT) {
+		// element
 		processElement(vnode, container);
-	} else if (isObject(vnode.type)) {
+	} else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+		// STATEFUL_COMPONENT
 		processComponent(vnode, container); // 组件的执行程序
 	}
 
@@ -52,11 +57,13 @@ function mountElement(vnode: any, container: any) {
 	const el = (vnode.el = document.createElement(vnode.type)) // 将el存起来   vnode是ele类型的 入口文件的div根元素
 
 	// string array
-	const { children } = vnode
-	if (typeof children === "string") {
+	const { children, shapeFlag } = vnode
+	if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+		// text_children
 		el.textContent = children;
-	} else if (Array.isArray(children)) {
+	} else if (shapeFlag & ShapeFlags.ARRRAY_CHILDREN) {
 		// 如果是array 说明传进来的是vnode 需要调用patch来继续判断ele还是conpontent
+		// array_children
 		mountChildren(vnode, el);
 	}
 	// props
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from "../shared/ShapeFlags";
 
 export function createVNode(type, props?, children?) {
 
@@ -5,9 +6,21 @@ export function createVNode(type, props?, children?) {
 		type,
 		props,
 		children,
+		shapeFlag: getShapeFlag(type),
 		el: null, // 把根节点 el 存起来
+	}
+	debugger
+	// children
+	if (typeof children === "string") {
+		vonode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+	} else if (Array.isArray(children)) {
+		vonode.shapeFlag |= ShapeFlags.ARRRAY_CHILDREN
 	}
 
 	return vonode;
 
+}
+
+function getShapeFlag(type: any) {
+	return typeof type === "string" ? ShapeFlags.ELEMENT : ShapeFlags.STATEFUL_COMPONENT
 }
